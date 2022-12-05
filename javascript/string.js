8c0fb34e204e0cb641139d03f4e5b15934682fc7
harshmangalam <harshdev8218@gmail.com>
2022-12-05 21:48:59 +0530

    lern string

--- javascript/string.js
+++ javascript/string.js
@@ -14,16 +14,55 @@ let myName3 = `my name`;
 let password = "myPass";
 let dbPass = "pass";
 
-
-
 // password === dbPass
 
 // const newPass = password.slice(2)
 // console.log(newPass)
 
-
 // console.log(password[password.length-1])
 
-const newstr = password.replace("my","")
+const newstr = password.replace("my", "");
 
-console.log(newstr)
+// console.log(newstr)
+
+// const fullName = "hArSh Mangalam";
+
+// console.log(fullName.toUpperCase())
+
+const search = "harsh mangalam";
+
+// console.log(fullName === search.toLocaleLowerCase());
+
+const firstName = "HarsH";
+const lastName = "Mangalam";
+
+
+// console.log(firstName.indexOf("H",1))
+
+
+// const fullName = firstName + " " +lastName;
+const fullName = `${firstName} ${lastName}`;
+
+// const fullName = firstName.concat(" ",lastName)
+
+const name = "                           harsh                                                   "
+
+// const paragramph =
+//   `Lorem Ipsum is simply dummy ${name} of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`;
+
+
+// console.log(fullName);
+
+
+// console.log(name.trim())
+// console.log(name.trimLeft())
+// console.log(name.trimRight())
+
+const id = 345
+
+
+// console.log(typeof id.toString())
+// console.log(id.toString())
+
+
+
