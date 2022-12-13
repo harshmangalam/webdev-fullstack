6b2bc9dadc1f8e8324b01c15e68c61aa6285d318
harshmangalam <harshdev8218@gmail.com>
2022-12-13 21:51:11 +0530

    learn array methods

--- javascript/array.js
+++ javascript/array.js
@@ -20,24 +20,24 @@
 
 // console.log(common[common.length - 1]);
 
-const users = [
-  {
-    id: 1,
-    name: "Harsh",
-    password: "123456",
-  },
-  {
-    id: 2,
-    name: "user 2",
-    password: "12345",
-  },
-];
+// const users = [
+//   {
+//     id: 1,
+//     name: "Harsh",
+//     password: "123456",
+//   },
+//   {
+//     id: 2,
+//     name: "user 2",
+//     password: "12345",
+//   },
+// ];
 
-users.push({
-  id: users.length + 1,
-  name: "user 3",
-  password: "user1234",
-});
+// users.push({
+//   id: users.length + 1,
+//   name: "user 3",
+//   password: "user1234",
+// });
 
 // console.log(users)
 
@@ -65,11 +65,11 @@ users.push({
 
 // users.shift()
 
-users.unshift({ id: 1, name: "user 1", password: "123" });
+// users.unshift({ id: 1, name: "user 1", password: "123" });
 
 // console.log(users);
 
-const a = [ 21,, 5, 7, 3];
+const a = [21, , 5, 7, 3];
 const b = [6, 7, 5];
 const c = [9, 10, 11];
 
@@ -80,7 +80,54 @@ const c = [9, 10, 11];
 // Array concat()
 
 // const newArr = a.splice(2,0,15,16,17);
-const newArr = a.splice(2,2);
-console.log(a)
+// const newArr = a.splice(2,2);
+// console.log(a)
 
-console.log(newArr)
+// console.log(newArr)
+
+// const nums = [1, 2, 3, 4, 5];
+
+// const twoTimesOfNums = nums.map((value) => value * 2);
+
+// console.log(nums);
+// console.log(twoTimesOfNums);
+
+// const newArray = [];
+
+// nums.forEach((value) => newArray.push(value * 2));
+
+// for (let i = 0; i <= nums.length - 1; i++) {
+//   console.log(nums[i]);
+// }
+
+// const filteredArr = nums.filter((value) => value > 3);
+
+// console.log(filteredArr);
+
+const users = [
+  {
+    name: "name 1",
+    role: "user",
+  },
+  {
+    name: "name 2",
+    role: "admin",
+  },
+  {
+    name: "name 3",
+    role: "user",
+  },
+];
+
+const usersRole = users.filter((user) => user.role === "user");
+
+console.log(usersRole);
+
+const nums = [1, 2, 3, 4, 5];
+
+const sum = nums.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  0
+);
+
+console.log(sum);
