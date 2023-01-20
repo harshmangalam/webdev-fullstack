6b201ba283220b97dd09ba807e6870c17ee9e0f0
harshmangalam <harshdev8218@gmail.com>
2023-01-20 21:36:50 +0530

    created user schema

---
+++ projects/instagram-backend-api/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: [3, "Name must be greater than 3 character"],
+      maxLength: [30, "Name must be lesser than 30 character"],
+    },
+
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      minLength: [3, "Name must be greater than 3 character"],
+      maxLength: [30, "Name must be lesser than 30 character"],
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minLength: [6, "Name must be greater than 6 character"],
+    },
+
+    bio: {
+      type: String,
+      maxLength: [150, "Name must be lesser than 150 character"],
+    },
+
+    isActive: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
--- projects/instagram-backend-api/server.js
+++ projects/instagram-backend-api/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import authRoute from "./routes/auth.route.js";
-
+import mongoose from "mongoose";
 const app = express();
 
 // it will parse incomming request json data
@@ -14,10 +14,16 @@ app.use(morgan("dev"));
 // it will handle auth related routes
 app.use("/auth", authRoute);
 
-function main() {
-  app.listen(3000, () => {
-    console.log("Server started");
-  });
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/instagram");
+    console.log("connected with database");
+    app.listen(3000, () => {
+      console.log("Server started");
+    });
+  } catch (error) {
+    console.log("database connection error:::", error);
+  }
 }
 
 main();
