a63078c6659d9237f77ad12c84f5b397f6eed9a2
harshmangalam <harshdev8218@gmail.com>
2023-01-21 21:43:28 +0530

    Added signup validation

--- projects/instagram-backend-api/controllers/auth.controller.js
+++ projects/instagram-backend-api/controllers/auth.controller.js
@@ -1,14 +1,18 @@
 import { Users } from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import { validationResult } from "express-validator";
 
 function login(req, res) {
   res.status(200).json({ done: true });
 }
 
 async function signup(req, res) {
-  const userData = req.body;
-
   // validation
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const userData = req.body;
 
   // check if username already exists
   const user = await Users.findOne({ username: userData.username }).exec();
--- projects/instagram-backend-api/routes/auth.route.js
+++ projects/instagram-backend-api/routes/auth.route.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { login, signup } from "../controllers/auth.controller.js";
+import { body } from "express-validator";
 const router = Router();
 
+const signupValidation = [
+  body("username").isString().isLength({ min: 3, max: 30 }),
+  body("password").isString().isLength({ min: 6 }),
+  body("name").isString().isLength({ min: 3, max: 30 }),
+];
+
 router.post("/login", login);
-router.post("/signup", signup);
+router.post("/signup", signupValidation, signup);
 
 export default router;
