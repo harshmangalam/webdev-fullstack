98990495413c8c49951095d74791dc86d89ab1ea
harshmangalam <harshdev8218@gmail.com>
2023-02-03 22:02:35 +0530

    added products list

--- reactjs/vite-react/src/App.jsx
+++ reactjs/vite-react/src/App.jsx
@@ -1,17 +1,66 @@
-import { useState } from "react";
-import GithubUser from "./GithubUser";
-
+import { useState, useEffect } from "react";
+import Product from "./Product";
 function App() {
-  const [show, setShow] = useState(false);
-  function toggle() {
-    setShow(!show);
-  }
-  return (
-    <div>
-      {show ? <GithubUser /> : "Empty"}
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
 
-      <button onClick={toggle}>Toggle</button>
-    </div>
+  const [products, setProducts] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newProduct = {
+      productName,
+      productPrice,
+    };
+
+    setProducts([...products, newProduct]);
+  }
+
+  return (
+    <>
+      <div>
+        <form method="POST" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="productName">Product Name</label>
+            <input
+              value={productName}
+              type="text"
+              id="productName"
+              name="productName"
+              onChange={(e) => setProductName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="productPrice">Product Price</label>
+            <input
+              value={productPrice}
+              type="number"
+              id="productPrice"
+              name="productPrice"
+              onChange={(e) => setProductPrice(e.target.value)}
+            />
+          </div>
+          <button type="submit">Add</button>
+        </form>
+
+        <div>
+          <ul>
+            {products.map((product) => (
+              <>
+                <li>
+                  <Product
+                    productName={product.productName}
+                    productPrice={product.productPrice}
+                  />
+                </li>
+
+                <div>Hello</div>
+              </>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div>jjsjs</div>
+    </>
   );
 }
 
---
+++ reactjs/vite-react/src/Product.jsx
@@ -0,0 +1,10 @@
+function Product({ productName, productPrice }) {
+  return (
+    <div>
+      <h1>{productName}</h1>
+      <h5>{productPrice}</h5>
+    </div>
+  );
+}
+
+export default Product;
